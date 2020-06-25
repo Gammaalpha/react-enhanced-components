1a63c6cc0b5a35d99805444f61aaae240d3733c5
Faiq Malik <faiqmalik2@gmail.com>
2020-06-25 17:35:44 -0400

    in progress

--- src/core/RichTextEditor/RichTextEditor.tsx
+++ src/core/RichTextEditor/RichTextEditor.tsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import { Editor, EditorState } from 'draft-js';
 import "draft-js/dist/Draft.css";
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core';
+import "./RichTextEditor.css"
+import { IRichText } from './model/RichText';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => createStyles({
     editorContainer: {
         width: '100%',
         minHeight: '350px',
@@ -12,22 +19,50 @@ const useStyles = makeStyles({
     },
     fullWidth: {
         minWidth: '100%'
+    },
+    flexGrow1: {
+        flexGrow: 1
+    },
+    toolbarBtn: {
+        marginRight: theme.spacing(2)
+    },
+    blackToolbar: {
+        backgroundColor: 'black'
     }
-})
+}))
 
-export const RichTextEditor = () => {
+export const RichTextEditor = (props: IRichText) => {
     const classes = useStyles()
     const [editorState, setEditorState] = useState(() => EditorState.createEmpty(),)
+    const editorRef = useRef<any>(null)
 
-    const onEditorStateChange = (editorState_: EditorState) => {
-        setEditorState(editorState_)
+    function focusEditor() {
+        editorRef.current?.focus();
+    }
+
+    const toolbar = () => {
+        return (
+            <div className={classes.flexGrow1}>
+                <AppBar position="static">
+                    <Toolbar className={props?.toolbarStyle ? props.toolbarStyle : classes.blackToolbar}>
+                        {/* <IconButton edge="start" className={classes.toolbarBtn} color="inherit" aria-label="menu">
+                        </IconButton>
+                        <Button color="inherit">Login</Button> */}
+
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
     }
 
     const editorRender = () => {
         return (
-            <div className={classes.editorContainer}>
+            <div className={classes.editorContainer} onClick={() => focusEditor()}>
+                {toolbar()}
                 <Editor
-                    editorState={editorState} onChange={(state) => onEditorStateChange(state)}
+                    ref={editorRef}
+                    editorState={editorState}
+                    onChange={(state) => setEditorState(state)}
                 />
             </div>
         )
@@ -37,7 +72,6 @@ export const RichTextEditor = () => {
     const render = () => {
         return (
             <div className={classes.fullWidth}>
-                Rich Text Editor
                 {editorRender()}
             </div>
         )
