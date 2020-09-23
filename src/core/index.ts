6373bf937f1d6d9bc59a96534637fbd466d7d7ef
Faiq Malik <faiqmalik2@gmail.com>
2020-09-23 16:50:23 -0400

    update

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,8 @@
 import { IHeader } from "./DataTable/model/header";
 export { DataTable } from "./DataTable/DataTable";
+export * from "./DataTable/DataTable";
+
 export { RichTextEditor } from "./RichTextEditor/RichTextEditor"
+export * from "./RichTextEditor/RichTextEditor"
+
 export declare type IHeaderProps = IHeader
