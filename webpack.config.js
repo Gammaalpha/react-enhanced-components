17c677dd0e2b28d5ad163eb5ef20b3496c3fa55f
Faiq Malik <faiqmalik2@gmail.com>
2020-11-16 00:03:09 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 // var webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/core/index.ts",
+  entry: "./core/index.ts",
   target: "node",
   mode: "production",
   output: {
